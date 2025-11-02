60f147c905b3be5bd99ab3f17f1c67f88ac10ac0
ash07-gif <ashporob@gmail.com>
2025-11-02 11:58:59 +0530

    Delete Exp 11 directory

--- Exp 11/Post.js
+++
@@ -1,8 +0,0 @@
-// Simple model shape documented for clarity (not enforced in JS)
-// {
-//   userId: number,
-//   id: number,
-//   title: string,
-//   body: string
-// }
-export default {};
--- Exp 11/posts.js
+++
@@ -1,6 +0,0 @@
-import client from './client';
-
-export async function fetchPosts() {
-  const resp = await client.get('/posts');
-  return resp.data;
-}
